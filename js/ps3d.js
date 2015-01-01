c4a75c61859ea3d3b74d7c8f1d83b90d8a9c8950
Andrew Nero <andrew.nero@gmail.com>
2014-12-31 22:23:18 -0800

    major anim preloader

--- js/ps3d.js
+++ js/ps3d.js
@@ -1,4 +1,4 @@
-"use strict";
+//"use strict";
 
 b4w.register("ps3d_main", function(exports, require) {
 
@@ -9,25 +9,27 @@ var m_data   = require("data");
 var m_main   = require("main");
 var m_scenes = require("scenes");
 var m_camera = require("camera");
+var m_preloader = require("preloader");
+//var m_version = require("version");
 //var m_camera_anim = require("camera_anim");
 //var m_material = require("material");
-var m_transform = require("transform");
+//var m_transform = require("transform");
 var m_sfx    = require("sfx");
 var _previous_selected_obj = null;
-
-
-var GIF_FRAMES_NUMBER = 24;
-var GIF_DELAY_TIME = 100;
-var VIDEO_DELAY_TIME = 1000/30;
-
+//var DEBUG = (m_version.type() === "DEBUG");
+var VIDEO_DELAY_TIME = 900/30;
+var PRELOADING = true;
+var state = 0;
 
 
 exports.init = function() {
     m_app.init({
-        canvas_container_id: "canvas3d", 
+        canvas_container_id: "main_canvas_container", 
         callback: init_cb,
+		//gl_debug: true,
         physics_enabled: false,
-        alpha: true
+        alpha: true,
+		//console_verbose: DEBUG
     });
 
 }
@@ -38,17 +40,35 @@ function init_cb(canvas_elem, success) {
         console.log("b4w init failure");
         return;
     }
-
+	
     m_app.enable_controls(canvas_elem);
+	//m_preloader.create_simple_preloader(canvas_elem);
     canvas_elem.addEventListener("mousedown", main_canvas_click, false);
+     m_preloader.create_advanced_preloader({
+        img_width: 165,
+        preloader_width: 460,
+        preloader_bar_id: "preloader_bar",
+        fill_band_id: "fill_band",
+        preloader_caption_id: "preloader_caption",
+        preloader_container_id: "preloader_container",
+        background_container_id: "background_image_container",
+        canvas_container_id: "main_canvas_container"
+    });
 
-    window.onresize = on_resize;
+    var preloader_frame = document.getElementById("preloader_frame");
+
+    preloader_frame.style.visibility = "visible"; 
+window.onresize = on_resize;
     on_resize();
-    load();
+    load();    
 }
 
 function load() {
-    m_data.load("ps3d.json", load_cb);
+        var p_cb = PRELOADING ? preloader_callback : null;
+    m_data.load("ps3d.json",
+                load_cb, p_cb, !true);
+				
+				
 }
 
 function load_cb(data_id) {
@@ -56,7 +76,9 @@ function load_cb(data_id) {
 	load_data();
 }
 
-
+function preloader_callback(percentage) {
+    m_preloader.update_preloader(percentage);
+}
 
 function on_resize() {
     var w = window.innerWidth;
@@ -74,13 +96,9 @@ function main_canvas_click(e) {
     var obj = m_scenes.pick_object(x, y);
 	
 	//var camobj = m_scenes.get_active_camera();
-	//var TotalWork = m_scenes.get_object_by_name("TotalWork");
+	var Rotation = m_scenes.get_object_by_name('Rotation');
 	//m_material.set_emit_factor(TotalWork, "krav", .2);
-	//m_material.set_emit_factor(TotalWork, "venezia", .2);
-	//m_material.set_emit_factor(TotalWork, "path", .2);
-	//m_material.set_emit_factor(TotalWork, "paoletti", .2);
-	//m_material.set_emit_factor(TotalWork, "phillys", .2);
-	//m_material.set_emit_factor(TotalWork, "awip", .5);
+	
 	
     if (obj) {
         if (_previous_selected_obj) {
@@ -89,61 +107,90 @@ function main_canvas_click(e) {
         }
         _previous_selected_obj = obj;
 		
-		if(obj.name=="venezia"){
-		    m_anim.apply_def(obj);
-			m_anim.play(obj);
-			setTimeout(function() { window.open('http://www.veneziamarble.com/', '_blank') }, 2000)
+		if(obj.name=="leftarrow"){
+		    if (state == 0){  //at welcome
+				m_anim.apply(Rotation, "WToWork");
+				state = 1;
+				}
+			else if (state == 1){ //at work
+				m_anim.apply(Rotation, "WorkToAnim");
+				state = 2;
+				}
+			else if (state == 2){ //at animation
+				m_anim.apply(Rotation, "AnimToMe");
+				state = 3;
+			}
+			else if (state == 3){ //at me
+				m_anim.apply(Rotation, "MeToW");
+				state = 0;
+			}						
+			m_anim.play(Rotation);	
 		}
-		else if (obj.name=="awip"){
-		   // m_anim.apply_def(obj);
-			//m_anim.apply(obj, "Plasmascape.001Action");
-		//	m_anim.play(obj);
-			//var TotalWork = m_scenes.get_object_by_name("Rotation");
-			//m_anim.apply_def(TotalWork);
-			//m_anim.play(TotalWork);
-			//m_transform.set_scale(obj, 100);
-			//m_transform.set_translation(obj,10,20,30);
-			//m_transform.set_translation_rel(obj,10,20,30,TotalWork);
-		//	m_transform.move_local(obj,10,20,30);
-			setTimeout(function() { window.open('http://www.awippersonaltraining.com/', '_blank') }, 20)
+		else if(obj.name=="rightarrow"){
+		    if (state == 0){  //at welcome
+				m_anim.apply(Rotation, "WToMe");
+				state = 3;
+				}
+			else if (state == 3){ //at me
+				m_anim.apply(Rotation, "MeToAnim");
+				state = 2;
+				}
+			else if (state == 2){ //at animation
+				m_anim.apply(Rotation, "AnimToWork");
+				state = 1;
+			}
+			else if (state == 1){ //at work
+				m_anim.apply(Rotation, "WorkToW");
+				state = 0;
+			}						
+			m_anim.play(Rotation);	
 		}
-		else if (obj.name=="phillys"){
-		    m_anim.apply_def(obj);
-			m_anim.play(obj);
-			setTimeout(function() { window.open('http://phillys.herokuapp.com/', '_blank') }, 20)
+		else if(state != -1) {//at work
+			if (obj.name=="venezia"){
+				m_anim.apply_def(obj);
+				m_anim.play(obj);
+				setTimeout(function() { window.open('http://www.veneziamarble.com/', '_blank') }, 2000)
+			}
+			else if (obj.name=="awip"){
+				setTimeout(function() { window.open('http://www.awippersonaltraining.com/', '_blank') }, 20)
+			}
+			else if (obj.name=="phillys"){
+				m_anim.apply_def(obj);
+				m_anim.play(obj);
+				setTimeout(function() { window.open('http://phillys.herokuapp.com/', '_blank') }, 20)
+			}
+			else if (obj.name=="paoletti plane"){
+				m_anim.apply_def(obj);
+				m_anim.play(obj);
+				setTimeout(function() { window.open('http://plaw.herokuapp.com/', '_blank') }, 20)
+			}
+			else if (obj.name=="venezia.001"){
+				m_anim.apply_def(obj);
+				m_anim.play(obj);
+				setTimeout(function() { window.open('http://phillys.herokuapp.com/', '_blank') }, 20)
+			}
+			else if (obj.name=="path"){
+				m_anim.apply_def(obj);
+				m_anim.play(obj);
+				setTimeout(function() { window.open('http://plaw.herokuapp.com/', '_blank') }, 20)
+			}
+			else if (obj.name=="krav plane"){
+				m_anim.apply_def(obj);
+				m_anim.play(obj);
+				setTimeout(function() { window.open('http://ctkrav.herokuapp.com/', '_blank') }, 20)
+			}
 		}
-		else if (obj.name=="paoletti plane"){
-		    m_anim.apply_def(obj);
-			m_anim.play(obj);
-			setTimeout(function() { window.open('http://plaw.herokuapp.com/', '_blank') }, 20)
-		}
-		else if (obj.name=="venezia.001"){
-		    m_anim.apply_def(obj);
-			m_anim.play(obj);
-			setTimeout(function() { window.open('http://phillys.herokuapp.com/', '_blank') }, 20)
-		}
-		else if (obj.name=="path"){
-		    m_anim.apply_def(obj);
-			m_anim.play(obj);
-			setTimeout(function() { window.open('http://plaw.herokuapp.com/', '_blank') }, 20)
-		}
-		else if (obj.name=="krav plane"){
-		    m_anim.apply_def(obj);
-			m_anim.play(obj);
-			setTimeout(function() { window.open('http://ctkrav.herokuapp.com/', '_blank') }, 20)
-		};
 			
-			m_anim.apply_def(obj);
-			m_anim.play(obj);
 
     }
 }
 
 function load_data() {
     var ctx_video = m_tex.get_canvas_texture_context("my_video5");
+	var ctx_image = m_tex.get_canvas_texture_context("my_image");
 
 
-    if (ctx_video) {
+     if (ctx_video) {
 
         var format = m_sfx.detect_video_container("");
         
@@ -161,8 +208,31 @@ function load_data() {
                 video_file.src="slider.mp4";
         } else
             console.log("Can not load the video.");
-    }
+    }  
 
+    if (ctx_image) {
+
+        var format2 = m_sfx.detect_video_container("");
+        
+        if (format2) {
+            var video_file2 = document.createElement('video');  
+            video_file2.autoplay = true;
+            video_file2.loop = true;
+			//video_file2.addEventListener("ended", function(e) {
+			//	this.currentTime = 0; 
+           //     this.play();
+            //}, false);
+            video_file2.addEventListener("loadeddata", function(e) {
+                draw_video_iter2(video_file2, ctx_image);
+            }, false);
+
+            if (format2 == "m4v")
+                video_file2.src="arial.mp4";
+            else
+                video_file2.src="arial.mp4";
+        } else
+            console.log("Can not load the video.");
+    }
 
 }
 
@@ -177,6 +247,16 @@ function draw_video_iter(video, context) {
     setTimeout(function() { draw_video_iter(video, context) }, VIDEO_DELAY_TIME);
 }
 
+function draw_video_iter2(video, context) {
+	var canvas = context.canvas;
+    var scale = canvas.width / video.videoWidth;
+    var scale_height = Math.round( scale * video.videoHeight );
+    var shift_position = Math.round((canvas.height - scale_height) / 2);
+    context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight, 
+            0, shift_position, canvas.width, scale_height);
+    m_tex.update_canvas_texture_context("my_image");
+    setTimeout(function() { draw_video_iter2(video, context) }, VIDEO_DELAY_TIME);
+}
 
 
 
